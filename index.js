d68012fb4804af6d9279fe05e48ffbfa4ee9b360
M Haidar Hanif <git@mhaidarhanif.com>
2017-10-17 11:47:16 +0700

    Connect to mongodb

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("HELLO")
+const mongo = require("mongodb").MongoClient
+
+// Connection URL
+const url = "mongodb://localhost:27017/myproject"
+
+// Use connect method to connect to the Server
+mongo.connect(url, (err, db) => {
+  console.log("Connected to MongoDB server")
+  db.close()
+})
